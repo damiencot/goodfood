7b1b330df624ebc2391b33e32af9261834f7e120
damiencot <damien.cot@gmail.com>
2022-06-26 11:25:34 +0200

    Initial version of goodfood generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/cesi/goodfood/config/DatabaseConfiguration.java
@@ -0,0 +1,16 @@
+package com.cesi.goodfood.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableJpaRepositories({ "com.cesi.goodfood.repository" })
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableTransactionManagement
+public class DatabaseConfiguration {}
---
+++ src/main/java/com/cesi/goodfood/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.cesi.goodfood.service.dto;
